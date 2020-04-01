ea7b63d6e3c7aa97953ea00088d7da9cc7a1c333
ajishtmohan <41186016+ajishtmohan@users.noreply.github.com>
2020-04-01 09:56:42 +0530

    service page added

---
+++ JS/custom.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    centerMode: true,
+    centerPadding: '0',
+    });
+});
