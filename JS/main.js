e74c93790fb2ced571205086849985773cc4b2d3
ajishtmohan <41186016+ajishtmohan@users.noreply.github.com>
2020-04-05 10:27:20 +0530

    as

--- JS/main.js
+++ JS/main.js
@@ -56,3 +56,14 @@ $(document).ready(function() {
             });
         });
 });
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() {
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('#roomhome').style.opacity = 1
+})
---
+++ JS/rooms.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+        var sidebar = $("#room1"); //get offset of second div
+        var offset = sidebar.offset().top; //check for top property
+        $(function () {
+            $(window).scroll(function () {
+                if ($(window).scrollTop() >= offset) {
+                    $(".room-sidebar").css("right","30px");
+                    $(".logo").css("height", "0px");
+                    $(".menu-button").css("border", "1px solid black");
+                    $(".menu-button").css("background-color", "rgba(0, 0, 0, 0.8)");
+                } else {
+                    $(".room-sidebar").css("right","-80px");
+                    $(".logo").css("height", "auto");
+                    $(".logo-menu").css("background-color", "#1c1c1c");
+                }
+            });
+        });
+});
